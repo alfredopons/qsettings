35b431f5488d605e4ecfbbbb4c0bf0b1b0b73161
Alfredo Pons Menargues <alfredo.pons@gmail.com>
2019-08-05 16:01:37 +0200

    Add support for read  qsetting vectors

--- main.cpp
+++ main.cpp
@@ -40,6 +40,16 @@ int main(int argc, char *argv[])
     QCommandLineOption keyOption(QStringList() << "k" << "key",QCoreApplication::translate("main", "Specify the key to search"), "machine/taxes");
     parser.addOption(keyOption);
 
+
+    QCommandLineOption vectorOption(QStringList() << "j" << "vector-key",QCoreApplication::translate("main", "Specify the key vector to searh"),"vector key");
+    parser.addOption(vectorOption);
+
+    QCommandLineOption vectorIndexOption(QStringList() << "i" << "vector-index",QCoreApplication::translate("main", "Specify the index in vector to searh"),"vector index");
+    parser.addOption(vectorIndexOption);
+
+    QCommandLineOption vectorSizeOption(QStringList() << "s" << "vector-size",QCoreApplication::translate("main", "Get size vector"));
+    parser.addOption(vectorSizeOption);
+
     QCommandLineOption lineOption(QStringList() << "n" << "no-newline",QCoreApplication::translate("main", "do not output the trailing newline"));
     parser.addOption(lineOption);
 
@@ -63,6 +73,98 @@ int main(int argc, char *argv[])
 
     QSettings settings (file, QSettings::IniFormat);
 
+    if (parser.isSet(vectorSizeOption)) {
+        if (!parser.isSet(vectorOption)) {
+            std::cerr << "I need a vector key: Option -j" << std::endl;
+            return EXIT_FAILURE;
+        }
+        QString key = parser.value(vectorOption);
+        if (key.isEmpty()) {
+            std::cerr << "Key Not found!: " << key.toStdString() << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        QStringList groups = settings.childGroups();
+        bool match = false;
+        for (const auto &i:groups){
+            if (i == key) {
+                match=true;
+                break;
+            }
+        }
+        if (!match) {
+            std::cerr << "Vector Key Not found!: " << key.toStdString() << std::endl;
+            return EXIT_FAILURE;
+        }
+        int size = settings.beginReadArray(key);
+        settings.endArray();
+        std::cout << size;
+        if (!parser.isSet(lineOption))
+            std::cout <<  std::endl;
+
+        return EXIT_SUCCESS;
+    }
+
+     if (parser.isSet(vectorOption)) {
+          if (!parser.isSet(keyOption)) {
+              std::cerr << "I need a Key: Option -k" << std::endl;
+              return EXIT_FAILURE;
+          }
+          if (!parser.isSet(vectorIndexOption)) {
+              std::cerr << "I need a index in vector: Option -i" << std::endl;
+              return EXIT_FAILURE;
+          }
+
+          QString key2 = parser.value(keyOption);
+          if (key2.isEmpty()) {
+              std::cerr << "Key Not found!: "  << std::endl;
+              return EXIT_FAILURE;
+          }
+
+          QString key = parser.value(vectorOption);
+          if (key.isEmpty()) {
+              std::cerr << "Vector Key Not found!: "  << std::endl;
+              return EXIT_FAILURE;
+          }
+
+          QStringList groups = settings.childGroups();
+          bool match = false;
+          for (const auto &i:groups){
+              if (i == key) {
+                  match=true;
+                  break;
+              }
+          }
+          if (!match) {
+              std::cerr << "Vector Key Not found!: " << key.toStdString() << std::endl;
+              return EXIT_FAILURE;
+          }
+
+          QString index = parser.value(vectorIndexOption);
+          if (index.isEmpty()) {
+              std::cerr << "index Not found!: " << std::endl;
+              return EXIT_FAILURE;
+          }
+          int num = index.toInt();
+          int size = settings.beginReadArray(key);
+          if (num >= size || num < 0) {
+              std::cerr << "Invalid index " << num;
+                  return EXIT_FAILURE;
+          }
+          settings.setArrayIndex(num);
+          if (key2.isEmpty() || !settings.contains(key2)) {
+              std::cerr << "Key " << key2.toStdString() << " Not found!" << std::endl;
+              settings.endArray();
+              return EXIT_FAILURE;
+          }
+          std::cout << settings.value(key2).toString().toStdString();
+          settings.endArray();
+          if (!parser.isSet(lineOption))
+              std::cout <<  std::endl;
+
+          return EXIT_SUCCESS;
+     }
+
     if (parser.isSet(keyOption)) {
         QString key = parser.value(keyOption);
         if (key.isEmpty() || !settings.contains(key)) {
@@ -74,7 +176,6 @@ int main(int argc, char *argv[])
         std::cout << settings.value(key).toString().toStdString();
         if (!parser.isSet(lineOption))
             std::cout <<  std::endl;
-
 
         return EXIT_SUCCESS;
     }
